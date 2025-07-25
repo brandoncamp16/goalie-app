df109866313ddfad238eb0364c0b7d7f7970943f
brandoncamp16 <brandoncamp16@gmail.com>
2025-07-25 13:53:56 -0400

    Updated the Unauthorized Page

--- frontend/src/components/Unauthorized.jsx
+++ frontend/src/components/Unauthorized.jsx
@@ -1,4 +1,6 @@
-import { useNavigate } from "react-router"
+import { Link, useNavigate } from "react-router"
+import { StepBack } from "lucide-react";
+import { LogOut } from 'lucide-react'
 
 const Unauthorized = () => {
     const navigate = useNavigate();
@@ -6,15 +8,35 @@ const Unauthorized = () => {
     const goBack = () => navigate(-1);
 
     return (
-        <section>
-            <h1>Unauthorized</h1>
-            <br />
-            <p>You do not have access to the requested page.</p>
-            <div className="flexGrow">
-                <button onClick={goBack}>Go Back</button>
+        <div className='min-h-screen bg-base-200'>
+        <div className='container mx-auto px-4 py-8'>
+            <div className='max-w-2xl mx-auto'>
+            <h1 className='text-3xl font-bold text-primary font-mono tracking-tighter text-center h-10 mb-20'>Goalie</h1>
+            <div className='card bg-base-100'>
+                <div className='card-body'>
+                <h2 className='card-title text-2xl mb-4'>Unauthorized</h2>
+                    <div className='card-body flex-auto'>
+                        <div className="card-actions">
+                            <div className="float-left">
+                            <Link to={goBack} className='btn btn-primary' onClick={goBack}>
+                                {"Go Back"}
+                                <StepBack className='size-5' />
+                            </Link>
+                            </div>
+                            <div className="float-right">
+                            <Link to={"/login"} className='btn btn-primary' onClick={goBack}>
+                                {"Change User"}
+                                <LogOut className='size-5' />
+                            </Link>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                </div>
             </div>
-        </section>
-    )
+            </div>
+        </div>
+    );
 }
 
 export default Unauthorized
